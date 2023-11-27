41e58474587c2515c6eaacf4484bc76771fd3732
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-11-27 21:31:11 +0200

    10. Linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle - checks the cycle
+ * @list: list
+ * Return:0
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *s = list, f = list;
+
+	while (f && f->next)
+	{
+		s = s->next;
+		f = f->next->next;
+		if (s == f)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
