d613a5a045a0367dba6f68a968b071286c703d92
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-11-28 22:02:39 +0200

    5. 00...99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-print("0" + str(0), end='')
-i = 1
+i = 0
 while i < 100:
-    if i < 10:
+    if i == 0:
+        message = "0" + str(i)
+    elif i < 10:
         message = ", 0".format() + str(i)
     else:
         message = ", ".format() + str(i)
     print(message, end='')
     i += 1
+print()
