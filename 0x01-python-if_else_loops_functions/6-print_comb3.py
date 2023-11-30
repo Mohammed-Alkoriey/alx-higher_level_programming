54235ce61b37907252e2cb89399b9ab983930235
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-11-30 16:21:42 +0200

    update

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for digit1 in range(10):
+    for digit2 in range(digit1 + 1, 10):
+        print(f"{digit1:02d}, {digit2:02d}", end=", ")
+
+# Print a newline character after the last combination
+print()
