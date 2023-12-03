1a67ae387d5c6482c748e2e622611dd77424acd5
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-12-03 22:13:50 +0200

    8. More returns

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,6 +4,6 @@ def multiple_returns(sentence):
     leng = len(sentence)
     if leng > 0:
         first_char = sentence[0]
-        return leng, first_char
     else:
-        return None
+        first_char = "None"
+    return leng, first_char
