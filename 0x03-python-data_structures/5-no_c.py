a57fe3b7a15da4930bc832c1fc57fc6d85faa556
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-12-03 21:36:09 +0200

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,6 +3,6 @@
 def no_c(my_string):
     new_string = ""
     for char in my_string:
-        if char is not 'c' and char is not 'C':
+        if char != 'c' and char != 'C':
             new_string += char
     return new_string
