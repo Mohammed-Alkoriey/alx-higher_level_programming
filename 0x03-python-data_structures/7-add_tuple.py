f28fc5036d9b48e1ff41e4d8bb7450e359cc1cf1
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-12-03 22:04:07 +0200

    7. Tuples addition

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a += (0 , 0)
+    tuple_b += (0 , 0)
+    num1 = tuple_a[0] + tuple_b[0]
+    num2 = tuple_a[1] + tuple_b[1]
+    result = (num1 , num2)
+    return result
