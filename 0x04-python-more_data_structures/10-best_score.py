73233c84574ac1987a33890ad327eb97d467dec8
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-12-06 23:14:42 +0200

    10. Best score

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    best = 0
+    best_key = ""
+    for key, value in a_dictionary.items():
+        if value > best:
+            best = value
+            best_key = key
+        else:
+            continue
+    return best_key
