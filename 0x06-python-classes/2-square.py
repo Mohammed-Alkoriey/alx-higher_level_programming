1d7e9b601cc5071b0d24602d08ee2fa24697bcb0
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-12-19 21:00:22 +0200

    2. Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square:
+    """The attriputes of the square class"""
+    def __init__(self, size=0):
+        """creates a copy of square class"""
+        self.__size = size
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
