bc4ca9f65fb4ee53dacc0e14f30fe383272e2851
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2023-12-19 21:13:34 +0200

    4. Access and update private attribute

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a Square class"""
+
+
+class Square:
+    """The attriputes of the square class"""
+    def __init__(self, size=0):
+        """creates a copy of square class"""
+        self.__size = size
+
+    def area(self):
+        result = self.__size * self.__size
+        return result
+
+    @property
+    def size(self):
+        """Get"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Set"""
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
