e59e3895bce747308f5828e07607df20dbce0561
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2024-01-05 00:41:29 +0200

    3. String representation

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -43,14 +43,15 @@ class Rectangle():
             return 0
         else:
             return (2 * self.width) + (2 * self.height)
+
     def __str__(self):
         hashed = []
 
         if self.width == 0 or self.height == 0:
             return ''
 
-        for(x in range(0, self.height)):
-            for(y in range(0, self.width)):
+        for (x in range(0, self.height)):
+            for (y in range(0, self.width)):
                 hashed.append("#")
             hashed.append("\n")
 
