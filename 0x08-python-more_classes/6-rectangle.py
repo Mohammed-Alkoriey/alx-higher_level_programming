f165e79d69c1cafc4aaaac9a78fbe58051f83356
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2024-01-05 00:56:37 +0200

    6. How many instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,6 +5,7 @@
 class Rectangle():
     """Square class properties"""
     number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
