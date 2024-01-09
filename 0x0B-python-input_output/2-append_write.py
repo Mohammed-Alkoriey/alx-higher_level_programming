a6fe3828c8f17b73c4690eda2140ddeaca8b0e7b
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2024-01-09 23:51:51 +0200

    2. Append to a file

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -3,7 +3,7 @@
 """append a string into a file"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """appending into a file"""
     with open(filename, "a", encoding="utf-8") as file1:
         return file1.write(text)
