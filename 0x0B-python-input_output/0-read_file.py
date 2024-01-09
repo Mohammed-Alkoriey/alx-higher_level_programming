9efa93dbf3bb51f93efb73981b2f793dc3ef0bb0
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2024-01-09 23:17:28 +0200

    0. Read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    with open(filename, "r", encoding="utf-8") as file1:
+    with open(filename, "r", encoding="UTF-8") as file1:
         file_content = file1.read()
-
-        print(file_content)
+        print(file_content, end="")
