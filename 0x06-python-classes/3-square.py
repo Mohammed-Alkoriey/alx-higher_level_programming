78d12d569a95c038790b405760c77e7326212b96
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2024-01-10 20:43:23 +0200

    repo update

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -11,7 +11,7 @@ class Square:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-
+        
     def area(self):
         result = self.__size * self.__size
         return result
