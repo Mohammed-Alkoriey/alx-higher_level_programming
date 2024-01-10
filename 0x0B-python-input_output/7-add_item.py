413e1a7d70019c7b54807803706c9ba46da76d06
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2024-01-10 02:20:52 +0200

    7. Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,9 +9,13 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-if __name__ == "__main__":
-    list = load_from_json_file('add_item.json')
 
-    for x in range(1, sys.argc + 1):
-        list.append(sys.argv[x])
-    save_to_json_file(list, "add_item.json")
+list = list(sys.argv[1:])
+
+try:
+    od = load_from_json_file("add_item.json")
+except Exception:
+    od = []
+
+od.extend(list)
+save_to_json_file(od, "add_item.json")
