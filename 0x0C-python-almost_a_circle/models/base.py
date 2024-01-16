5a273c05f9f2ab2ea2579e3aeda2d3732dcc9f25
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2024-01-16 20:35:27 +0200

    1. Base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+"""Base module"""
+
+
+class Base:
+    """a class named base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        '''init function'''
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
