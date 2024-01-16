df44d16362964d02be154afe32976cf9e9ee6267
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2024-01-16 22:48:26 +0200

    3. Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -20,10 +20,6 @@ class Rectangle(Base):
 
     @width.setter
     def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must ba an integer")
-        if value <= 0:
-            raise ValueError("width must be > 0")
         self.__width = value
 
     @property
@@ -32,10 +28,6 @@ class Rectangle(Base):
 
     @height.setter
     def height(self, value):
-        if not isinstance(value, int):
-            raise TypeError("height must ba an integer")
-        if value <= 0:
-            raise ValueError("height must be > 0")
         self.__height = value
 
     @property
@@ -44,10 +36,6 @@ class Rectangle(Base):
 
     @x.setter
     def x(self, value):
-        if not isinstance(value, int):
-            raise TypeError("x must ba an integer")
-        if value < 0:
-            raise ValueError("x must be >= 0")
         self.__x = value
 
     @property
@@ -56,8 +44,13 @@ class Rectangle(Base):
 
     @y.setter
     def y(self, value):
-        if not isinstance(value, int):
-            raise TypeError("y must ba an integer")
-        if value < 0:
-            raise ValueError("y must be >= 0")
         self.__y = value
+
+    def attr_valid(self, name, value, equal=True):
+        '''Method for validating the attributes'''
+        if type(value) != int:
+            raise TypeError("{} must be an integer".format(name))
+        if equal and value < 0:
+            raise ValueError("{} must be >= 0".format(name))
+        elif not equal and value <= 0:
+            raise ValueError("{} must be > 0".format(name))
