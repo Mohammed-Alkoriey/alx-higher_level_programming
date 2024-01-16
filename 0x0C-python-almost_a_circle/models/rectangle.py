f557c27607b18d1a2eea6e11e4ced56ff9bef2e3
Mohammed-Alkoriey <mohammedalkorieywork@gmail.com>
2024-01-16 21:50:43 +0200

    2. First Rectangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """module for Rectangle"""
-from models.base import Base
+from base import Base
 
 
 class Rectangle(Base):
     """Rectangle class"""
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """init function"""
         super().__init__(id)
         self.width = width
         self.height = height
